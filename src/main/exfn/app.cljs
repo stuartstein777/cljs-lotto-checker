b145543d24f8fd106a3e9b9cdb17adb34ab55259
Stuart Stein <qmstuart@gmail.com>
2021-05-30 00:28:46 +0100

    added link to repo to index moved styles into main.css

--- src/main/exfn/app.cljs
+++ src/main/exfn/app.cljs
@@ -84,47 +84,40 @@
 ;; -- Reagent Forms ------------------------------------------------------------------
 (defn letters []
   (let [selected-letters @(rf/subscribe [:letters])]
-    [:div {:style {:margin-bottom 20}}
+    [:div.letters
      [:h3 "Select upto 18 letters"]
      (let [letters "abcdefghijklmnopqrstuvwxyz"
            a-to-m (->> letters
-                       (take 13)
-                       (map identity))
+                       (take 13))
            n-to-z (->> letters
-                       (drop 13)
-                       (map identity))]
+                       (drop 13))]
        [:div
         [:div.flex-container
          (for [[k letter] (keyed-collection a-to-m)]
-           [:div {:key k
-                  :style {:cursor           :pointer
-                                   :background-color (if (selected-letters letter) :yellow "#f1f1f1")}
-                  :on-click #(rf/dispatch [:toggle-letter letter])}
-            [:label {:style {:cursor :pointer}} letter]])]
+           [:div.pointable {:key k
+                            :style {:background-color (if (selected-letters letter) :yellow "#f1f1f1")}
+                            :on-click #(rf/dispatch [:toggle-letter letter])}
+            [:label.pointable letter]])]
         [:div.flex-container
          (for [[k letter] (keyed-collection n-to-z)]
-           [:div  {:key k
-                   :style    {:cursor           :pointer
-                              :background-color (if (selected-letters letter) :yellow "#f1f1f1")}
-                   :on-click #(rf/dispatch [:toggle-letter letter])}
-            [:label {:style {:cursor :pointer}} letter]])]
+           [:div.pointable  {:key      k
+                             :style    {:background-color (if (selected-letters letter) :yellow "#f1f1f1")}
+                             :on-click #(rf/dispatch [:toggle-letter letter])}
+            [:label.pointable  letter]])]
         [:div (str "Selected " (count selected-letters) " letters")]])]))
 
 (defn word-editor []
   (let [winners @(rf/subscribe [:winners])]
     [:div
      [:h3 "Words"]
-     [:div {:style {:visibility (if (>= (count winners) 3) :visible :collapse)
-                    :height (if (>= (count winners) 3) 40 0)}}
-      [:h1 {:style {:color :orange
-                    :text-shadow "-1px 0 red, 0 1px red, 1px 0 red, 0 -1px red"}}
-       "You are a winner!"]]
+     [:div.winner {:style {:visibility (if (>= (count winners) 3) :visible :collapse)
+                           :height (if (>= (count winners) 3) 40 0)}}
+      [:h1.winner-label "You are a winner!"]]
      [:div
       [:label "Enter word: "]
-      [:input {:type "text"
+      [:input.word-input {:type "text"
                :on-change #(rf/dispatch-sync [:word-change (-> % .-target .-value)])
-               :value @(rf/subscribe [:current-word])
-               :style {:margin 10}}]
+               :value @(rf/subscribe [:current-word])}]
       [:button.btn.btn-primary
        {:on-click #(rf/dispatch [:add-word])}
        [:i.fas.fa-plus]]
@@ -142,15 +135,9 @@
          [:div [:button.delete.btn
                 {:on-click #(rf/dispatch [:delete w])}
                 [:i.fas.fa-trash-alt]]
-          [:i.fas.fa-star {:style {:color :orange
-                                   :margin-right 5
-                                   :text-shadow "-1px 0 red, 0 1px red, 1px 0 red, 0 -1px red"
-                                   :visibility (if (winners w) :visible :hidden)}}]
+          [:i.fas.fa-star.star {:style {:visibility (if (winners w) :visible :hidden)}}]
           w
-          [:i.fas.fa-star {:style {:color :orange
-                                   :margin-left 5
-                                   :text-shadow "-1px 0 red, 0 1px red, 1px 0 red, 0 -1px red"
-                                   :visibility (if (winners w) :visible :hidden)}}]]])]]))
+          [:i.fas.fa-star.star {:style {:visibility (if (winners w) :visible :hidden)}}]]])]]))
 
 ;; -- App -------------------------------------------------------------------------
 (defn app []
@@ -192,5 +179,3 @@
   (start))
 
 (defonce initialize (rf/dispatch-sync [:initialize]))       ; dispatch the event which will create the initial state. 
-
-
