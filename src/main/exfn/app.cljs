ca93f0f35dea88fddd1e8c9af369f75390aebdbe
Stuart Stein <qmstuart@gmail.com>
2021-06-14 17:57:47 +0100

    fixing accidentally overwtiting with wrong repo...

--- src/main/exfn/app.cljs
+++ src/main/exfn/app.cljs
@@ -1,14 +1,10 @@
 (ns exfn.app
   (:require [reagent.dom :as dom]
             [re-frame.core :as rf]
-            [exfn.subscriptions]
-            [exfn.events]
-            [goog.string :as gstring]
             [clojure.string :as str]
             [clojure.set :as set]))
 
-<<<<<<< HEAD
-;; -- Helpers -------------------------------------------------------
+;; -- Helpers ------------------------------------------------------------------------------------
 (defn check [letters words]
   (let [letters (set letters)]
     (->> words
@@ -18,7 +14,7 @@
 (defn keyed-collection [col]
   (map vector (iterate inc 0) col))
 
-;;-- Events and Effects ---------------------------------------------
+;;-- Events and Effects --------------------------------------------------------------------------
 (rf/reg-event-db
  :initialize
  (fn [_ _]
@@ -65,7 +61,7 @@
          (assoc :words new-words)
          (assoc :winners (check (db :letters) new-words))))))
 
-;; -- Subscriptions --------------------------------------------------
+;; -- Subscriptions ------------------------------------------------------------------
 (rf/reg-sub
  :letters
  (fn [db _]
@@ -86,16 +82,13 @@
  (fn [db _]
    (db :winners)))
 
-;; -- Reagent Forms --------------------------------------------------
+;; -- Reagent Forms ------------------------------------------------------------------
 (defn letters-row [letters selected-letters]
   [:div.flex-container
    (for [[k letter] (keyed-collection letters)]
-     [:div.letter-box.pointable
-      {:key      k
-       :style    {:background-color (if (selected-letters letter)
-                                      "#EE6C4D"
-                                      "#f1f1f1")}
-       :on-click #(rf/dispatch [:toggle-letter letter])}
+     [:div.letter-box.pointable {:key      k
+                                 :style    {:background-color (if (selected-letters letter) "#EE6C4D" "#f1f1f1")}
+                                 :on-click #(rf/dispatch [:toggle-letter letter])}
       [:label.pointable.letter letter]])])
 
 (defn letters []
@@ -106,8 +99,7 @@
        [:div
         [letters-row a-to-m selected-letters]
         [letters-row n-to-z selected-letters]
-        [:div.selected-count
-         (str "Selected " (count selected-letters) " letters")]])]))
+        [:div.selected-count (str "Selected " (count selected-letters) " letters")]])]))
 
 (defn word-editor []
   (let [winners @(rf/subscribe [:winners])
@@ -117,88 +109,45 @@
      [:h3 "Words"]
      [:div
       [:label "Enter word: "]
-      [:input.word-input
-       {:type      "text"
-        :on-change #(rf/dispatch-sync [:word-change (-> % .-target .-value)])
-        :value     @(rf/subscribe [:current-word])}]
+      [:input.word-input {:type "text"
+                          :on-change #(rf/dispatch-sync [:word-change (-> % .-target .-value)])
+                          :value @(rf/subscribe [:current-word])}]
       [:button.btn.btn-primary
        {:on-click #(rf/dispatch [:add-word])
         :disabled (= "" (str/trim current-word))}
        [:i.fas.fa-plus]]
       [:label.winners-count
        (str (count winners) " Winners!")]]
-     [:h1.winner-indicator.winner
-      {:style {:visibility (if winner? :visible :collapse)
-               :height     (if winner? 40 0)}}
+     [:h1.winner-indicator.winner {:style {:visibility (if winner? :visible :collapse)
+                                           :height (if winner? 40 0)}}
       "You are a winner!"]]))
 
 (defn star [hidden?]
-  [:i.fas.fa-star.star.winner-indicator
-   {:style {:visibility (if hidden? :visible :hidden)}}])
+  [:i.fas.fa-star.star.winner-indicator {:style {:visibility (if hidden? :visible :hidden)}}])
 
 (defn words []
   (let [words @(rf/subscribe [:words])
         winners @(rf/subscribe [:winners])]
-=======
-(defn bmi-widget [bmi]
-  [:div.bmi
-   [:div.row.bmi-container
-    [:div.col.col-lg-2.bmi-underweight "15"]
-    [:div.col.col-lg-4.bmi-normal "18.5"]
-    [:div.col.col-lg-3.bmi-overweight "25"]
-    [:div.col.col-lg-3.bmi-obese "30"]]
-   [:div.indicator.bmi-indicator {:style {:left (str (* (/ (- bmi 15) 20.0) 100) "%")}}
-    [:i.fas.fa-sort-up]]])
-
-
-(defn get-current-weight-from-stats [days]
-  (->> days
-       (map (fn [{:keys [date weight]}]
-              {:date   (js/Date. date)
-               :weight weight}))
-       (sort-by :date >)
-       (first)
-       :weight))
-
-(defn calc-bmi [height weight]
-  (/ weight (* height height)))
-
-(defn weight-tracker []
-  (let [{:keys [target-weight days]} @(rf/subscribe [:daily-stats])
-        current-weight (get-current-weight-from-stats days)
-        bmi (calc-bmi 1.75 current-weight)]
->>>>>>> 0153277fd7c5f22d1bc186c6cc29b686d2705ef6
     [:div
-     [:div.row
-      [:div.col.col-lg-9]
-      [:div.col.col-lg-3
-       [:div.weight
-        [:div.row.weight-row
-         [:div.col.col-md-9  "Current Weight"]
-         [:div.col.col-md-3.weight-value current-weight " kg"]]
-        [:div.row.weight-row
-         [:div.col.col-md-9 "Target Weight"]
-         [:div.col.col-md-3.weight-value target-weight " kg"]]
-        [:div.row.weight-row
-         [:div.col.col-md-9 "To Lose"]
-         [:div.col.col-md-3.weight-value (- current-weight target-weight) " kg"]]
-        [:div.row
-         [:div.col.col-md-9 "BMI"]
-         [:div.col.col-md-3.weight-value (gstring/format "%.2f" bmi)]]
-        [:div.row
-         [:div.col.col-md-12
-          [bmi-widget bmi]]]]]]]))
+     [:ul.no-bullets
+      (for [[k w] (keyed-collection words)]
+        (let [winner? (winners w)]
+          [:li {:key k}
+           [:div [:button.delete.btn
+                  {:on-click #(rf/dispatch [:delete w])}
+                  [:i.fas.fa-times.delete]]
+            w
+            [star winner?]]]))]]))
 
-<<<<<<< HEAD
-;; -- App ------------------------------------------------------------
-=======
->>>>>>> 0153277fd7c5f22d1bc186c6cc29b686d2705ef6
+;; -- App -------------------------------------------------------------------------
 (defn app []
   [:div.container
-   [:h1 "Fitness Tracker"]
-   [weight-tracker]])
+   [:h1 "Letters Lotto Checker"]
+   [letters]
+   [word-editor]
+   [words]])
 
-;; -- After-Load -----------------------------------------------------
+;; -- After-Load --------------------------------------------------------------------
 ;; Do this after the page has loaded.
 ;; Initialize the initial db state.
 (defn ^:dev/after-load start
@@ -209,4 +158,4 @@
 (defn ^:export init []
   (start))
 
-(defonce initialize (rf/dispatch-sync [:initialize]))
+(defonce initialize (rf/dispatch-sync [:initialize]))       ; dispatch the event which will create the initial state. 
--- src/main/exfn/helpers.cljs
+++
@@ -1,4 +0,0 @@
-(ns exfn.helpers)
-
-(defn keyed-collection [col]
-  (map vector (iterate inc 0) col))
